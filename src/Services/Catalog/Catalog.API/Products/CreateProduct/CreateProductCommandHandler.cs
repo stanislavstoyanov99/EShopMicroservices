1829a9a83ee4f7bca260c932da663c1d190c5e9f
Stanislav Stoyanov <slavkata_99@abv.bg>
2024-08-12 17:20:07 +0300

    Add postgres db and create docker compose file

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -1,20 +1,15 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler
+    internal class CreateProductCommandHandler(IDocumentSession session)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            // Business logic to create a product
-
             var product = new Product
             {
                 Name = command.Name,
@@ -24,9 +19,10 @@ namespace Catalog.API.Products.CreateProduct
                 Price = command.Price
             };
 
-            // Save to DB : TODO
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
 
-            return new CreateProductResult(Guid.NewGuid());
+            return new CreateProductResult(product.Id);
         }
     }
 }
