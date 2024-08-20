70d509e3ba8638c07356e47aed42dd9a7b93fdad
Stanislav Stoyanov <slavkata_99@abv.bg>
2024-08-20 15:56:14 +0300

    Implement custom exceptions

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -26,7 +26,7 @@
 
             if (product is null)
             {
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(command.Id);
             }
 
             session.Delete<Product>(command.Id);
--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
@@ -41,7 +41,7 @@
 
             if (product is null)
             {
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(command.Id);
             }
 
             product.Name = command.Name;
