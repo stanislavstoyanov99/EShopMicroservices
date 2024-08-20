51de6d337fd030eea7363ab1421e0f1987ada553
Stanislav Stoyanov <slavkata_99@abv.bg>
2024-08-20 13:55:02 +0300

    Implement custom command handler validations

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -9,10 +9,19 @@
     {
         public CreateProductCommandValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
-            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
-            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
-            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+            RuleFor(command => command.Name)
+                .NotEmpty()
+                .WithMessage("Name is required")
+                .Length(2, 150)
+                .WithMessage("Name must be between 2 and 150 characters");
+
+            RuleFor(command => command.Category).NotEmpty().WithMessage("Category is required");
+
+            RuleFor(command => command.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+
+            RuleFor(command => command.Price)
+                .GreaterThan(0)
+                .WithMessage("Price must be greater than 0");
         }
     }
 
--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -4,6 +4,14 @@
 
     public record DeleteProductResult(bool IsSuccess);
 
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+        }
+    }
+
     internal class DeleteProductCommandHandler(
         IDocumentSession session,
         ILogger<DeleteProductCommandHandler> logger)
@@ -12,6 +20,14 @@
         public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
         {
             logger.LogInformation($"DeleteProductCommandHandler.Handle called with {command}");
+
+            var product = await session
+                .LoadAsync<Product>(command.Id, cancellationToken);
+
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
 
             session.Delete<Product>(command.Id);
             await session.SaveChangesAsync(cancellationToken);
