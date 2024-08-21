e6016f52a27ae94d90389d17af1be92dda64b0fb
Stanislav Stoyanov <slavkata_99@abv.bg>
2024-08-21 16:31:57 +0300

    Add docker-compose all configuration and remove commented code

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,7 +1,5 @@
 ﻿namespace Catalog.API.Products.GetProductById
 {
-    // public record GetProductByIdRequest();
-
     public record GetProductByIdResponse(Product Product);
 
     public class GetProductByIdEndpoint : ICarterModule
