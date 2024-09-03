3812590acbe38ce656fcc41d3bc84c78797816e2
Stanislav Stoyanov <slavkata_99@abv.bg>
2024-09-03 11:47:22 +0300

    Implement DeleteBasket handler and endpoint

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,26 @@
 ﻿namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketHandler
+    public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+    public record DeleteBasketResult(bool IsSuccess);
+
+    public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
     {
+        public DeleteBasketCommandValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+
+    public class DeleteBasketHandler :
+        ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+        {
+            // TODO: delete basket from database and cache
+            // session.Delete<Product>(command.Id);
+
+            return new DeleteBasketResult(true);
+        }
     }
 }
