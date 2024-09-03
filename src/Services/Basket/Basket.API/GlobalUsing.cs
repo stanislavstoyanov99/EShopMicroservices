86750715a0415f8c0f30088ad3b74999a3497398
Stanislav Stoyanov <slavkata_99@abv.bg>
2024-09-03 12:24:32 +0300

    Register Carter and MediatR

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -4,3 +4,4 @@ global using Carter;
 global using Mapster;
 global using MediatR;
 global using FluentValidation;
+global using BuildingBlocks.Behaviors;
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 // Add services to the container.
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
