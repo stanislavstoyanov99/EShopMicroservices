8405734b4abeef7022600307094cceb0548f39e8
Stanislav Stoyanov <slavkata_99@abv.bg>
2024-09-16 13:48:47 +0300

    Add DDD abstractions for Ordering.Domain

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid EventId => Guid.NewGuid();
+
+        DateTime OccurredOn => DateTime.Now;
+
+        string EventType => GetType().AssemblyQualifiedName;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Domain.Abstractions
+{
+    public interface IEntity<T> : IEntity
+    {
+        T Id { get; set; }
+    }
+
+    public interface IEntity
+    {
+        DateTime? CreatedAt { get; set; }
+
+        string? CreatedBy { get; set; }
+
+        DateTime? LastModified { get; set; }
+
+        string? LastModifiedBy { get; set; }
+    }
+}
