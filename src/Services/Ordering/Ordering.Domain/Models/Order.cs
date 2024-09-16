01414fd58087986b914d9530517104670807e19a
Stanislav Stoyanov <slavkata_99@abv.bg>
2024-09-16 15:52:28 +0300

    Add Order domain model

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Order : Aggregate<Guid>
+    {
+        private readonly List<OrderItem> _orderItems = new();
+
+        public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+        public Guid CustomerId { get; private set; } = default!;
+
+        public string Name { get; private set; } = default!;
+
+        public Address ShippingAddress { get; private set; } = default!;
+
+        public Address BillingAddress { get; private set; } = default!;
+
+        public Payment Payment { get; private set; } = default!;
+
+        public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+
+        public decimal TotalPrice
+        {
+            get => OrderItems.Sum(x => x.Price * x.Quantity);
+            private set { }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class OrderItem : Entity<Guid>
+    {
+        public OrderItem(Guid orderId, Guid productId, int quantity, decimal price)
+        {
+            Id = orderId;
+            ProductId = productId;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public Guid ProductId { get; private set; } = default!;
+
+        public int Quantity { get; private set; } = default!;
+
+        public decimal Price { get; private set; } = default!;
+    }
+}
