9f43ac4251975cb3929716314dd4e792ca1ac6c3
Stanislav Stoyanov <slavkata_99@abv.bg>
2024-10-02 14:42:59 +0300

    Seed SQL Server OrderDb using EF Core

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure.Data.Extensions
+{
+    public static class DatabaseExtensions
+    {
+        public static async Task InitialiseDatabaseAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+            await SeedAsync(context);
+        }
+
+        private static async Task SeedAsync(ApplicationDbContext context)
+        {
+            await SeedCustomerAsync(context);
+            //await SeedProductAsync(context);
+            //await SeedOrderItemsAsync(context);
+        }
+
+        private static async Task SeedCustomerAsync(ApplicationDbContext context)
+        {
+            if (!await context.Customers.AnyAsync())
+            {
+                await context.Customers.AddRangeAsync(InitialData.Customers);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Infrastructure.Data.Extensions
+{
+    public class InitialData
+    {
+        public static IEnumerable<Customer> Customers =>
+            new List<Customer>
+            {
+                Customer.Create(CustomerId.Of(new Guid("58c49479-ec65-4de2-86e7-033c546291aa")), "stanislav", "stanislav@gmail.com"),
+                Customer.Create(CustomerId.Of(new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d")), "john", "john@gmail.com")
+            };
+    }
+}
