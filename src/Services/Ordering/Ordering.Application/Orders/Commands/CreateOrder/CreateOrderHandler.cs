e27efd9492df40ead8f38dd2403761c116aa27c4
Stanislav Stoyanov <slavkata_99@abv.bg>
2024-10-16 17:50:35 +0300

    Add CreateOrderCommand and Handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public class CreateOrderHandler
+        : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    {
+        public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            // Create Order entity from command object
+            // Save to database
+            // Return result
+
+            throw new NotImplementedException();
+        }
+    }
+}
