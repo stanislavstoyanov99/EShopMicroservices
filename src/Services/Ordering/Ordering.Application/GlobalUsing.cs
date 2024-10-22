3ed9ffe343dc3f1183b1cc38d56d4800e0ba768d
Stanislav Stoyanov <slavkata_99@abv.bg>
2024-10-22 13:00:11 +0300

    Implement Query handler - GetOrdersByName

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -8,3 +8,4 @@ global using FluentValidation;
 global using MediatR;
 global using Microsoft.Extensions.Logging;
 global using Ordering.Domain.Events;
+global using Microsoft.EntityFrameworkCore;
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,62 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName
+{
+    public class GetOrdersByNameHandler(IApplicationDbContext dbContext)
+        : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+    {
+        public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await dbContext.Orders
+                .Include(o => o.OrderItems)
+                .AsNoTracking()
+                .Where(o => o.Name.Value.Contains(query.Name))
+                .OrderBy(o => o.Name)
+                .ToListAsync(cancellationToken);
+
+            var orderDtos = ProjectToOrderDto(orders);
+
+            return new GetOrdersByNameResult(orderDtos);
+        }
+
+        private List<OrderDto> ProjectToOrderDto(List<Order> orders)
+        {
+            var result = new List<OrderDto>();
+
+            foreach (var order in orders)
+            {
+                var orderDto = new OrderDto(
+                    Id: order.Id.Value,
+                    CustomerId: order.CustomerId.Value,
+                    OrderName: order.Name.Value,
+                    ShippingAddress: new AddressDto(
+                        order.ShippingAddress.FirstName,
+                        order.ShippingAddress.LastName,
+                        order.ShippingAddress.EmailAddress,
+                        order.ShippingAddress.AddressLine,
+                        order.ShippingAddress.Country,
+                        order.ShippingAddress.State,
+                        order.ShippingAddress.ZipCode),
+                    BillingAddress: new AddressDto(
+                        order.BillingAddress.FirstName,
+                        order.BillingAddress.LastName,
+                        order.BillingAddress.EmailAddress,
+                        order.BillingAddress.AddressLine,
+                        order.BillingAddress.Country,
+                        order.BillingAddress.State,
+                        order.BillingAddress.ZipCode),
+                    Payment: new PaymentDto(
+                        order.Payment.CardName,
+                        order.Payment.CardNumber,
+                        order.Payment.Expiration,
+                        order.Payment.CVV,
+                        order.Payment.PaymentMethod),
+                    Status: order.Status,
+                    OrderItems: order.OrderItems
+                        .Select(oi => new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList());
+
+                result.Add(orderDto);
+            }
+
+            return result;
+        }
+    }
+}
