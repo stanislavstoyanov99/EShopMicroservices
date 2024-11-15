abdcf06b3121e6a10bd61cb69b248cf379cfc77a
Stanislav Stoyanov <slavkata_99@abv.bg>
2024-11-15 15:43:17 +0200

    Add shopping cart UI view

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,6 +1,9 @@
 namespace Shopping.Web.Pages
 {
-    public class IndexModel(ICatalogService catalogService, ILogger<IndexModel> logger)
+    public class IndexModel(
+        ICatalogService catalogService,
+        IBasketService basketService,
+        ILogger<IndexModel> logger)
         : PageModel
     {
         public IEnumerable<ProductModel> ProductList { get; set; } = new LinkedList<ProductModel>();
@@ -12,6 +15,28 @@ namespace Shopping.Web.Pages
             ProductList = result.Products;
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
+        {
+            logger.LogInformation("Add to cart button clicked");
+
+            var productResponse = await catalogService.GetProduct(productId);
+
+            var basket = await basketService.LoadUserBasket();
+
+            basket.Items.Add(new ShoppingCartItemModel
+            {
+                ProductId = productId,
+                ProductName = productResponse.Product.Name,
+                Price = productResponse.Product.Price,
+                Quantity = 1,
+                Color = "Black"
+            });
+
+            await basketService.StoreBasket(new StoreBasketRequest(basket));
+
+            return RedirectToPage("Cart");
         }
     }
 }
--- src/WebApps/Shopping.Web/Services/IBasketService.cs
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -1,4 +1,6 @@
-﻿namespace Shopping.Web.Services
+﻿using System.Net;
+
+namespace Shopping.Web.Services
 {
     public interface IBasketService
     {
@@ -13,5 +15,28 @@
 
         [Post("/basket-service/basket/checkout")]
         Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
+
+        public async Task<ShoppingCartModel> LoadUserBasket()
+        {
+            // Get Basket if not exist create new basket with default logged in user name
+            var userName = "swn";
+            ShoppingCartModel basket;
+
+            try
+            {
+                var getBasketResponse = await GetBasket(userName);
+                basket = getBasketResponse.Cart;
+            }
+            catch (ApiException apiException) when (apiException.StatusCode == HttpStatusCode.NotFound)
+            {
+                basket = new ShoppingCartModel
+                {
+                    UserName = userName,
+                    Items = []
+                };
+            }
+
+            return basket;
+        }
     }
 }
